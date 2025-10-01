d776205e5b1072081d3fd3897699e3f402371175
jack <amirhossein4898@gmail.com>
2025-10-01 12:42:46 +0330

    Complete downloader library. add test file

---
+++ tests/downloader_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"imageExtractor/internal/downloader"
+	"time"
+)
+
+func main() {
+	urls := []string{"https://app.akharinkhabar.ir/images/2025/09/30/5548fd5b-80af-406b-8d48-4355e4574447.jpeg"}
+	d := downloader.NewHTTPDownloader(nil, 5)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	files, err := d.Download(ctx, urls, "./out")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("downloaded:", files)
+}
